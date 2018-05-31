f73810e910137047e06161b23ac7476716443e03
Simon Bailey <simon@pelli.co.uk>
2018-05-31 15:49:35 +0100

    hts-1006 content changes to check eligibility (#73)

--- test/uk/gov/hmrc/helptosavestridefrontend/controllers/StrideControllerSpec.scala
+++ test/uk/gov/hmrc/helptosavestridefrontend/controllers/StrideControllerSpec.scala
@@ -487,7 +487,7 @@ class StrideControllerSpec
         mockAuthFail()
         val result = controller.handleDetailsConfirmed(FakeRequest())
         status(result) shouldBe SEE_OTHER
-        redirectLocation(result) shouldBe Some("/stride/sign-in?successURL=http%3A%2F%2F%2Fhelp-to-save%2Fdigitally-excluded%2Fdetails-confirmed&origin=help-to-save-stride-frontend")
+        redirectLocation(result) shouldBe Some("/stride/sign-in?successURL=http%3A%2F%2F%2Fhelp-to-save%2Fhmrc-internal%2Fdetails-confirmed&origin=help-to-save-stride-frontend")
       }
 
     }
