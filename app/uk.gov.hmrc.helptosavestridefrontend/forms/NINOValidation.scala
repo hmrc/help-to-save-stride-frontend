758c14d1b849469b7653ae5b6d7a851433b5518a
Andrew Chung <andrew.chung@digital.hmrc.gov.uk>
2018-09-26 15:43:16 +0100

    hts-1379 - NINO suffix is now mandatory in UI (#108)

--- app/uk.gov.hmrc.helptosavestridefrontend/forms/NINOValidation.scala
+++ app/uk.gov.hmrc.helptosavestridefrontend/forms/NINOValidation.scala
@@ -36,7 +36,7 @@ object NINOValidation {
     private def validatedFromBoolean[A](a: A)(predicate: A ⇒ Boolean, ifFalse: ⇒ String): ValidatedNel[String, A] =
       if (predicate(a)) Valid(a) else invalid(ifFalse)
 
-    val ninoRegex: Regex = """^((?!(BG|GB|KN|NK|NT|TN|ZZ)|(D|F|I|Q|U|V)[A-Z]|[A-Z](D|F|I|O|Q|U|V))[A-Z]{2})[0-9]{6}[A-D]?$""".r
+    val ninoRegex: Regex = """^((?!(BG|GB|KN|NK|NT|TN|ZZ)|(D|F|I|Q|U|V)[A-Z]|[A-Z](D|F|I|O|Q|U|V))[A-Z]{2})[0-9]{6}[A-D]{1}$""".r
 
     override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], String] = {
       val validation: Validated[NonEmptyList[String], String] =
