f0827a4f9b99749e2a15b636916df6646c238e44
Marcus Cumming <marcus.cumming@digital.hmrc.gov.uk>
2018-10-19 10:05:42 +0100

    Modified port logging

--- selenium-system-test/src/test/scala/htsstride/steps/Steps.scala
+++ selenium-system-test/src/test/scala/htsstride/steps/Steps.scala
@@ -61,9 +61,13 @@ private[steps] trait Steps extends ScalaDsl with EN with Matchers {
       println("Marcus printing running services")
       println(runningServices)
 
-      val ports = Process("netstat -tulpn").!!
+      val port9041 = Process("lsof -i :9041").!!
+      val port9042 = Process("lsof -i :9042").!!
+      val port9043 = Process("lsof -i :9043").!!
       println("Marcus printing ports")
-      println(ports)
+      println(port9041)
+      println(port9042)
+      println(port9043)
 
       _driver.foreach(_.quit())
       _driver = None
