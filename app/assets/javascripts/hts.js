b29b0410a6c23e690eb6607f4c67706fe4d67c93
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2023-03-01 11:39:19 +0000

    [DLS-6901][DLS-6903] remove cancel/submit js and pass error state to layout

--- app/assets/javascripts/hts.js
+++ app/assets/javascripts/hts.js
@@ -1,7 +1,34 @@
-;(function(){
+;(function(window, document){
     const GOVUK = window.GOVUK;
     if (GOVUK) {
         const showHideContent = new GOVUK.ShowHideContent();
         showHideContent.init();
     }
-})(window);
+
+    const checkbox = document.querySelector('[name="confirm_eligible"]')
+    const button = document.querySelector('#continue')
+
+    if (checkbox && button) {
+        const enableButton = function() {
+            button.removeAttribute('aria-disabled')
+            button.removeAttribute('disabled')
+        }
+        const disableButton = function() {
+            button.setAttribute('aria-disabled', 'true')
+            button.setAttribute('disabled', 'disabled')
+        }
+
+        checkbox.addEventListener('change', function(e) {
+            const isChecked = e.currentTarget.checked;
+            if (isChecked) {
+                enableButton()
+            } else {
+                disableButton()
+            }
+        })
+
+        if (checkbox.checked) {
+            enableButton()
+        }
+    }
+})(window, document);
