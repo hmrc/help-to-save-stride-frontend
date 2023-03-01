a0c47b98644c19f42ef6066a26a2f45e7f4d621c
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2023-03-01 14:28:16 +0000

    [DLS-6903][DLS-6901] customer not eligible and enter customer details (#203)

--- app/assets/javascripts/hts.js
+++ app/assets/javascripts/hts.js
@@ -1,4 +1,34 @@
-;(function(){
-    var showHideContent = new GOVUK.ShowHideContent();
-    showHideContent.init();
-})();
+;(function(window, document){
+    const GOVUK = window.GOVUK;
+    if (GOVUK) {
+        const showHideContent = new GOVUK.ShowHideContent();
+        showHideContent.init();
+    }
+
+    const checkbox = document.querySelector('[name="confirm_eligible"]')
+    const button = document.querySelector('#continue')
+
+    if (checkbox && button) {
+        const enableButton = function() {
+            button.removeAttribute('aria-disabled')
+            button.removeAttribute('disabled')
+        }
+        const disableButton = function() {
+            button.setAttribute('aria-disabled', 'true')
+            button.setAttribute('disabled', 'disabled')
+        }
+
+        checkbox.addEventListener('change', function(e) {
+            const isChecked = e.currentTarget.checked;
+            if (isChecked) {
+                enableButton()
+            } else {
+                disableButton()
+            }
+        })
+
+        if (checkbox.checked) {
+            enableButton()
+        }
+    }
+})(window, document);
