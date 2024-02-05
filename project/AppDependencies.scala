41fa467e373beeb74429e807865f6abb743d2719
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-02-05 16:37:59 +0000

    DLS-9710: Updated cats-core to version 2.9.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,21 @@ import sbt.*
 object AppDependencies {
   val playVersion = "play-28"
   val hmrcBootstrapVersion = "7.15.0"
+  val hmrcMongoVersion = "0.73.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "0.73.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
-    "org.typelevel"     %% "cats-core"                  % "2.8.0",
+    "org.typelevel"     %% "cats-core"                  % "2.9.0",
     "com.github.kxbmap" %% "configs"                    % "0.6.1",
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % s"7.23.0-$playVersion"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" % "flexmark-all"              % "0.35.10"            % scope,
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-28"  % "0.73.0"             % scope,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion     % scope,
     "org.scalatest"        %% "scalatest"                % "3.2.9"              % scope,
     "org.scalamock"        %% "scalamock"                % "5.2.0"              % scope,
     "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"        % scope,
