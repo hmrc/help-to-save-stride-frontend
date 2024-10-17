035a25734e1787d40d5645fe23b58eb47dadf918
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-10-17 14:47:16 +0100

    DLS-10983: Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% s"domain-$playVersion"             % "9.0.0",
-    "org.typelevel"     %% "cats-core"                        % "2.10.0",
+    "org.typelevel"     %% "cats-core"                        % "2.12.0",
     "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "10.11.0"
   )
 
@@ -17,6 +17,6 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion     % scope,
     "org.scalatestplus" %% "scalacheck-1-17"               % "3.2.18.0"           % scope,
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"  % hmrcBootstrapVersion % scope,
-    "org.mockito"       %% "mockito-scala"                 % "1.17.31"            % scope
+    "org.mockito"       %% "mockito-scala"                 % "1.17.37"            % scope
   )
 }
