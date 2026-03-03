8b7dcfab852f5d3c69a48b50ce1bb5007f0ef91a
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2026-03-03 17:13:32 +0000

    [DLS-12762]Upgrade bootstrap play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
   val playVersion = "play-30"
-  val hmrcBootstrapVersion = "10.5.0"
-  val hmrcMongoVersion = "2.11.0"
+  val hmrcBootstrapVersion = "10.7.0"
+  val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% s"domain-$playVersion"             % "13.0.0",
     "org.typelevel"     %% "cats-core"                        % "2.13.0",
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.25.0"
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.32.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
